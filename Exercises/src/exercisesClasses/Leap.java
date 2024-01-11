a2a8e0cf0093355dd0c68207ac6d653516a93aa4
Zelphh <matheusmelle2006@gmail.com>
2024-01-11 17:54:50 -0300

    Add Lead exercise

---
+++ Exercises/src/exercisesClasses/Leap.java
@@ -0,0 +1,16 @@
+package exercisesClasses;
+
+public class Leap {
+    public boolean isLeapYear(int year) {
+    	
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year %  400 == 0) {
+                    return true;
+                } else {return false;}
+            } else {return true;}
+        } else {return false;}
+    	
+    }
+    
+}
