fda0f50796b518919e71ffd1b42eacb4c6f0d9b9
Zelphh <matheusmelle2006@gmail.com>
2024-01-14 21:24:29 -0300

    Alterate the exercise Darts

--- Exercises/src/exercisesClasses/Darts.java
+++ Exercises/src/exercisesClasses/Darts.java
@@ -1,5 +1,21 @@
 package exercisesClasses;
 
-public class Darts {
+import java.lang.Math;
 
+public class Darts {
+    public int score(double xOfDart, double yOfDart) {
+        double d = Math.sqrt(Math.pow((0 - xOfDart), 2) + Math.pow((0 - yOfDart), 2));
+
+        int score;
+
+        if (d > 10) {
+            score = 0;
+        } else if (d > 5) {
+            score = 1;
+        } else if (d > 1 && d <= 5) {
+            score = 5;
+        } else {score = 10;}
+        
+        return score;
+    }
 }
